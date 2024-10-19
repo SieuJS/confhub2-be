e3b75ca556b107b1a663126dde323caf3b1c78fa
21120321 <gamefun206@gmail.com>
2024-10-19 16:03:59 +0700

    Initialize project with nest

---
+++ src/modules/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { CommonModule } from './common';
+import { PassengerModule } from './passenger/passenger.module';
+import { ConferenceModule } from './conference/conference.module';
+
+@Module({
+    imports: [
+        CommonModule,
+        PassengerModule,
+        ConferenceModule,
+    ]
+})
+export class ApplicationModule {}
