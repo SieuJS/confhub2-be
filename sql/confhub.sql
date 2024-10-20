590605426a9f6f313ac625181f0426e3a9026c89
Hoang Thi Khon <hkhon2803@gmail.com>
2024-10-20 09:21:59 +0700

    Rename the ranks_of_source table to source_ranks, add db to the confhub schema, and merge the call_for_papers table with the call_for_paper_details table into a call_for_paper table.

--- sql/confhub.sql
+++ sql/confhub.sql
@@ -1,6 +1,7 @@
+CREATE SCHEMA IF NOT EXISTS confhub;
 create extension if not exists "uuid-ossp";
 
-create table public.important_dates (
+create table confhub.important_dates (
     "id" uuid primary key default uuid_generate_v4(),
     "cfp_id" uuid,
     "date_type" text,
@@ -8,27 +9,21 @@ create table public.important_dates (
     "status" text
 );
 
-create table public.rank_of_cfp (
+create table confhub.rank_of_cfp (
     "id" uuid primary key default uuid_generate_v4(),
     "rank_id" uuid,
     "cfp_id" uuid,
     "year" numeric
 );
 
-create table public.call_for_papers (
+create table confhub.call_for_papers (
     "id" uuid primary key default uuid_generate_v4(),
     "conference_id" uuid,
     "content" text,
     "link" text,
     "avg_rating" float,
     "owner" text,
-    "status" text,
-    "view_count" numeric
-);
-
-create table call_for_paper_details (
-    "id" uuid primary key default uuid_generate_v4(),
-    "cfp_id" uuid,
+    "view_count" numeric,
     "location" text,
     "country" text,
     "start_date" date,
@@ -37,32 +32,33 @@ create table call_for_paper_details (
     "status" boolean
 );
 
-create table sources (
+
+create table confhub.sources (
     "id" uuid primary key default uuid_generate_v4(),
     "name" text,
     "link" text
 );
 
-create table ranks_of_source (
+create table confhub.source_ranks (
     "id" uuid primary key default uuid_generate_v4(),
     "source_id" uuid,
     "rank" text,
     "value" numeric
 );
 
-create table fields_of_research (
+create table confhub.fields_of_research (
     "id" uuid primary key default uuid_generate_v4(),
     "code" text,
     "name" text
 );
 
-create table conferences (
+create table confhub.conferences (
     "id" uuid primary key default uuid_generate_v4(),
     "name" text, 
     "acronym" text
 );
 
-create table journals (
+create table confhub.journals (
     "id" uuid primary key default uuid_generate_v4(),
     "country" text,
     "name" text,
@@ -74,40 +70,41 @@ create table journals (
     "email_submission" text
 );
 
-create table journal_rank_footprints (
+create table confhub.journal_rank_footprints (
     "id" uuid primary key default uuid_generate_v4(),
     "journal_id" uuid,
     "rank_id" uuid,
+    "for_id" uuid,
     "year" numeric
 );
 
-create table conference_rank_footprints (
+create table confhub.conference_rank_footprints (
     "id" uuid primary key default uuid_generate_v4(),
     "conference_id" uuid,
     "rank_id" uuid,
+    "for_id" uuid,
     "year" numeric
 );
 
--- foreign keys for call_for_paper_details table
-alter table call_for_paper_details add constraint fk_detail_of_cfp foreign key (cfp_id) references call_for_papers(id);
-
 -- foreign keys for important_dates table
-alter table important_dates add constraint fk_important_date_of_cfp foreign key (cfp_id) references call_for_papers(id);
+alter table confhub.important_dates add constraint fk_important_date_of_cfp foreign key (cfp_id) references confhub.call_for_papers(id);
 
 -- foreign keys for call_for_papers table 
-alter table call_for_papers add constraint fk_cfp_belong_conference foreign key (conference_id) references conferences(id);
+alter table confhub.call_for_papers add constraint fk_cfp_belong_conference foreign key (conference_id) references confhub.conferences(id);
 
--- rank_of_cfp table foreign keys
-alter table rank_of_cfp add constraint fk_cfp_has_rank foreign key (cfp_id) references call_for_papers(id);
-alter table rank_of_cfp add constraint fk_rank_of_cfp foreign key (rank_id) references ranks_of_source(id);
+-- rank_of_cfc table foreign keys
+alter table confhub.rank_of_cfp add constraint fk_cfp_has_rank foreign key (cfp_id) references confhub.call_for_papers(id);
+alter table confhub.rank_of_cfp add constraint fk_rank_of_cfc foreign key (rank_id) references confhub.source_ranks(id);
 
--- foreign keys for ranks_of_source table
-alter table ranks_of_source add constraint fk_rank_of_source foreign key (source_id) references sources(id);
+-- foreign keys for source_ranks table
+alter table confhub.source_ranks add constraint fk_rank_of_source foreign key (source_id) references confhub.sources(id);
 
 -- foreign keys for conference_rank_footprints table
-alter table conference_rank_footprints add constraint fk_conference_rank_footprints foreign key (conference_id) references conferences(id);
-alter table conference_rank_footprints add constraint fk_rank_of_conference foreign key (rank_id) references ranks_of_source(id);
+alter table confhub.conference_rank_footprints add constraint fk_conference_rank_footprints foreign key (conference_id) references confhub.conferences(id);
+alter table confhub.conference_rank_footprints add constraint fk_rank_of_conference foreign key (rank_id) references confhub.source_ranks(id);
+alter table confhub.conference_rank_footprints add constraint fk_conference_rank_footprints_for foreign key (for_id) references confhub.fields_of_research(id);
 
 -- foreign keys for journal_rank_footprints table
-alter table journal_rank_footprints add constraint fk_journal_rank_footprints foreign key (journal_id) references journals(id);
-alter table journal_rank_footprints add constraint fk_rank_of_journal foreign key (rank_id) references ranks_of_source(id);
+alter table confhub.journal_rank_footprints add constraint fk_journal_rank_footprints foreign key (journal_id) references confhub.journals(id);
+alter table confhub.journal_rank_footprints add constraint fk_rank_of_journal foreign key (rank_id) references confhub.source_ranks(id);
+alter table confhub.journal_rank_footprints add constraint fk_journal_rank_footprints_for foreign key (for_id) references confhub.fields_of_research(id);
