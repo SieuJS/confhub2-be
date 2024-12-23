1f413eb0a25e37fd3ff4fc7d4d61cd0f05f92681
21120321 <gamefun206@gmail.com>
2024-12-23 15:07:24 +0700

    chore : implement service lint

--- src/modules/conf-api-lint/conf-api-lint.module.ts
+++ src/modules/conf-api-lint/conf-api-lint.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PaginateModule } from '../paginate/paginate.module';
 import { CommonModule } from '../common';
 import { ConferenceApiLintController } from './controller';
+import { ConferenceApiLintService } from './service/conference-api-lint.service';
 
 @Module({
     imports : [PaginateModule, CommonModule],
     controllers : [ConferenceApiLintController],
-    providers : [],
+    providers : [ConferenceApiLintService],
     exports : [] 
 
 })
